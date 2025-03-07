c3d810ea7e7adf8c05d209e8d764818708017ad4
Le Quoc Nam <lequocnam012@gmail.com>
2025-03-08 01:07:18 +0700

    savenight0803

---
+++ TNS_TOEICAdmin/TNS_TOEICAdmin/TNS_TOEICPart1/Areas/TOEICPart1/Models/AnswerDataAccess.cs
@@ -0,0 +1,238 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Data;
+
+namespace TNS_TOEICPart1.Areas.TOEICPart1.Models
+{
+    public class AnswerDataAccess
+    {
+        public class Part1_Answer_Info
+        {
+            #region [Field Name]
+            private string _QuestionKey = "";
+            private string _AnswerKey = "";
+            private string _AnswerText = "";
+            private bool _AnswerCorrect;
+            private int _RecordStatus = 0;
+            private DateTime? _CreatedOn = null;
+            private string _CreatedBy = "";
+            private string _CreatedName = "";
+            private DateTime? _ModifiedOn = null;
+            private string _ModifiedBy = "";
+            private string _ModifiedName = "";
+            private string _Message = "";
+            private bool _IsNewRecord = true;
+            private string _Status = "";
+            #endregion
+
+            #region [Constructor]
+            public Part1_Answer_Info()
+            {
+                Guid zNewID = Guid.NewGuid();
+                _AnswerKey = zNewID.ToString();
+                _IsNewRecord = true;
+                _Status = "OK";
+            }
+
+            public Part1_Answer_Info(string AnswerKey)
+            {
+                if (string.IsNullOrEmpty(AnswerKey?.Trim()))
+                {
+                    Guid zNewID = Guid.NewGuid();
+                    _AnswerKey = zNewID.ToString();
+                    _IsNewRecord = true;
+                    _Status = "OK";
+                    return;
+                }
+
+                string zSQL = "SELECT * FROM [dbo].[TEC_Part1_Answer] WHERE AnswerKey = @AnswerKey AND RecordStatus != 99";
+                string zConnectionString = TNS.DBConnection.Connecting.SQL_MainDatabase;
+                using (SqlConnection zConnect = new SqlConnection(zConnectionString))
+                {
+                    zConnect.Open();
+                    try
+                    {
+                        using (SqlCommand zCommand = new SqlCommand(zSQL, zConnect))
+                        {
+                            zCommand.CommandType = CommandType.Text;
+                            zCommand.Parameters.Add("@AnswerKey", SqlDbType.UniqueIdentifier).Value = new Guid(AnswerKey);
+                            using (SqlDataReader zReader = zCommand.ExecuteReader())
+                            {
+                                if (zReader.HasRows)
+                                {
+                                    zReader.Read();
+                                    _QuestionKey = zReader["QuestionKey"].ToString();
+                                    _AnswerKey = zReader["AnswerKey"].ToString();
+                                    _AnswerText = zReader["AnswerText"].ToString();
+                                    _AnswerCorrect = (bool)zReader["AnswerCorrect"];
+                                    _RecordStatus = int.Parse(zReader["RecordStatus"].ToString());
+                                    if (zReader["CreatedOn"] != DBNull.Value) _CreatedOn = (DateTime)zReader["CreatedOn"];
+                                    _CreatedBy = zReader["CreatedBy"].ToString();
+                                    _CreatedName = zReader["CreatedName"].ToString();
+                                    if (zReader["ModifiedOn"] != DBNull.Value) _ModifiedOn = (DateTime)zReader["ModifiedOn"];
+                                    _ModifiedBy = zReader["ModifiedBy"].ToString();
+                                    _ModifiedName = zReader["ModifiedName"].ToString();
+                                    _IsNewRecord = false;
+                                    _Status = "OK";
+                                    _Message = "Record found!";
+                                }
+                                else
+                                {
+                                    _Status = "ERROR";
+                                    _Message = "No record found!";
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception Err)
+                    {
+                        _Status = "ERROR";
+                        _Message = Err.ToString();
+                    }
+                }
+            }
+            #endregion
+
+            #region [Properties]
+            public string QuestionKey { get => _QuestionKey; set => _QuestionKey = value; }
+            public string AnswerKey { get => _AnswerKey; set => _AnswerKey = value; }
+            public string AnswerText { get => _AnswerText; set => _AnswerText = value; }
+            public bool AnswerCorrect { get => _AnswerCorrect; set => _AnswerCorrect = value; }
+            public int RecordStatus { get => _RecordStatus; set => _RecordStatus = value; }
+            public DateTime? CreatedOn { get => _CreatedOn; set => _CreatedOn = value; }
+            public string CreatedBy { get => _CreatedBy; set => _CreatedBy = value; }
+            public string CreatedName { get => _CreatedName; set => _CreatedName = value; }
+            public DateTime? ModifiedOn { get => _ModifiedOn; set => _ModifiedOn = value; }
+            public string ModifiedBy { get => _ModifiedBy; set => _ModifiedBy = value; }
+            public string ModifiedName { get => _ModifiedName; set => _ModifiedName = value; }
+            public bool IsNewRecord { get => _IsNewRecord; }
+            public string Status { get => _Status; }
+            public string Message { get => _Message; set => _Message = value; }
+            #endregion
+
+            #region [CRUD Methods]
+            public void Create()
+            {
+                if (string.IsNullOrEmpty(_QuestionKey) || !Guid.TryParse(_QuestionKey, out _))
+                {
+                    _Status = "ERROR";
+                    _Message = "Invalid QuestionKey format: " + _QuestionKey;
+                    return;
+                }
+
+                string zSQL = @"INSERT INTO [dbo].[TEC_Part1_Answer] (
+                    QuestionKey, AnswerKey, AnswerText, AnswerCorrect, RecordStatus, CreatedBy, CreatedName, ModifiedBy, ModifiedName
+                ) VALUES (
+                    @QuestionKey, @AnswerKey, @AnswerText, @AnswerCorrect, @RecordStatus, @CreatedBy, @CreatedName, @ModifiedBy, @ModifiedName
+                )";
+                string zConnectionString = TNS.DBConnection.Connecting.SQL_MainDatabase;
+                using (SqlConnection zConnect = new SqlConnection(zConnectionString))
+                {
+                    zConnect.Open();
+                    try
+                    {
+                        using (SqlCommand zCommand = new SqlCommand(zSQL, zConnect))
+                        {
+                            zCommand.CommandType = CommandType.Text;
+                            zCommand.Parameters.Add("@QuestionKey", SqlDbType.UniqueIdentifier).Value = new Guid(_QuestionKey);
+                            zCommand.Parameters.Add("@AnswerKey", SqlDbType.UniqueIdentifier).Value = new Guid(_AnswerKey);
+                            zCommand.Parameters.Add("@AnswerText", SqlDbType.NVarChar).Value = _AnswerText ?? (object)DBNull.Value;
+                            zCommand.Parameters.Add("@AnswerCorrect", SqlDbType.Bit).Value = _AnswerCorrect;
+                            zCommand.Parameters.Add("@RecordStatus", SqlDbType.Int).Value = _RecordStatus;
+                            zCommand.Parameters.Add("@CreatedBy", SqlDbType.UniqueIdentifier).Value = string.IsNullOrEmpty(_CreatedBy) ? DBNull.Value : new Guid(_CreatedBy);
+                            zCommand.Parameters.Add("@CreatedName", SqlDbType.NVarChar).Value = _CreatedName ?? (object)DBNull.Value;
+                            zCommand.Parameters.Add("@ModifiedBy", SqlDbType.UniqueIdentifier).Value = string.IsNullOrEmpty(_ModifiedBy) ? DBNull.Value : new Guid(_ModifiedBy);
+                            zCommand.Parameters.Add("@ModifiedName", SqlDbType.NVarChar).Value = _ModifiedName ?? (object)DBNull.Value;
+                            zCommand.ExecuteNonQuery();
+                            _Status = "OK";
+                            _Message = "Answer created successfully";
+                        }
+                    }
+                    catch (Exception Err)
+                    {
+                        _Status = "ERROR";
+                        _Message = Err.ToString();
+                    }
+                }
+            }
+
+            public void Update()
+            {
+                if (string.IsNullOrEmpty(_QuestionKey) || !Guid.TryParse(_QuestionKey, out _) || !Guid.TryParse(_AnswerKey, out _))
+                {
+                    _Status = "ERROR";
+                    _Message = "Invalid QuestionKey or AnswerKey format";
+                    return;
+                }
+
+                string zSQL = @"UPDATE [dbo].[TEC_Part1_Answer] SET 
+                    AnswerText = @AnswerText, 
+                    AnswerCorrect = @AnswerCorrect, 
+                    RecordStatus = @RecordStatus, 
+                    ModifiedOn = GETDATE(), 
+                    ModifiedBy = @ModifiedBy, 
+                    ModifiedName = @ModifiedName 
+                    WHERE AnswerKey = @AnswerKey";
+                string zConnectionString = TNS.DBConnection.Connecting.SQL_MainDatabase;
+                using (SqlConnection zConnect = new SqlConnection(zConnectionString))
+                {
+                    zConnect.Open();
+                    try
+                    {
+                        using (SqlCommand zCommand = new SqlCommand(zSQL, zConnect))
+                        {
+                            zCommand.CommandType = CommandType.Text;
+                            zCommand.Parameters.Add("@AnswerKey", SqlDbType.UniqueIdentifier).Value = new Guid(_AnswerKey);
+                            zCommand.Parameters.Add("@AnswerText", SqlDbType.NVarChar).Value = _AnswerText ?? (object)DBNull.Value;
+                            zCommand.Parameters.Add("@AnswerCorrect", SqlDbType.Bit).Value = _AnswerCorrect;
+                            zCommand.Parameters.Add("@RecordStatus", SqlDbType.Int).Value = _RecordStatus;
+                            zCommand.Parameters.Add("@ModifiedBy", SqlDbType.UniqueIdentifier).Value = string.IsNullOrEmpty(_ModifiedBy) ? DBNull.Value : new Guid(_ModifiedBy);
+                            zCommand.Parameters.Add("@ModifiedName", SqlDbType.NVarChar).Value = _ModifiedName ?? (object)DBNull.Value;
+                            zCommand.ExecuteNonQuery();
+                            _Status = "OK";
+                            _Message = "Answer updated successfully";
+                        }
+                    }
+                    catch (Exception Err)
+                    {
+                        _Status = "ERROR";
+                        _Message = Err.ToString();
+                    }
+                }
+            }
+
+            public void Delete()
+            {
+                if (string.IsNullOrEmpty(_AnswerKey) || !Guid.TryParse(_AnswerKey, out _))
+                {
+                    _Status = "ERROR";
+                    _Message = "Invalid AnswerKey format";
+                    return;
+                }
+
+                string zSQL = "UPDATE [dbo].[TEC_Part1_Answer] SET RecordStatus = 99 WHERE AnswerKey = @AnswerKey";
+                string zConnectionString = TNS.DBConnection.Connecting.SQL_MainDatabase;
+                using (SqlConnection zConnect = new SqlConnection(zConnectionString))
+                {
+                    zConnect.Open();
+                    try
+                    {
+                        using (SqlCommand zCommand = new SqlCommand(zSQL, zConnect))
+                        {
+                            zCommand.Parameters.Add("@AnswerKey", SqlDbType.UniqueIdentifier).Value = new Guid(_AnswerKey);
+                            zCommand.ExecuteNonQuery();
+                            _Status = "OK";
+                            _Message = "Answer deleted successfully";
+                        }
+                    }
+                    catch (Exception Err)
+                    {
+                        _Status = "ERROR";
+                        _Message = Err.ToString();
+                    }
+                }
+            }
+            #endregion
+        }
+    }
+}
---
+++ TNS_TOEICAdmin/TNS_TOEICAdmin/TNS_TOEICPart1/Areas/TOEICPart1/Pages/Answer.cshtml.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TNS_TOEICPart1.Areas.TOEICPart1.Models;
+using System;
+
+namespace TNS_TOEICPart1.Areas.TOEICPart1.Pages
+{
+    [IgnoreAntiforgeryToken]
+    public class AnswerModel : PageModel
+    {
+        #region [Security]
+        public TNS.Auth.UserLogin_Info UserLogin;
+
+        private void CheckAuth()
+        {
+            UserLogin = new TNS.Auth.UserLogin_Info(User);
+            UserLogin.GetRole("TOEIC_Part1");
+            // For Testing
+            UserLogin.Role.IsRead = true;
+            UserLogin.Role.IsCreate = true;
+            UserLogin.Role.IsUpdate = true;
+            UserLogin.Role.IsDelete = true;
+        }
+        #endregion
+
+        public string AnswerKey { get; set; }
+        public string QuestionKey { get; set; }
+
+        public IActionResult OnGet(string Key, string QuestionKey)
+        {
+            CheckAuth();
+            if (UserLogin.Role.IsRead)
+            {
+                this.AnswerKey = Key;
+                // Kiểm tra định dạng GUID cho QuestionKey
+                if (!string.IsNullOrEmpty(QuestionKey) && !Guid.TryParse(QuestionKey, out _))
+                {
+                    return RedirectToPage("/QuestionList", new { error = "InvalidQuestionKey" });
+                }
+                this.QuestionKey = QuestionKey;
+                if (string.IsNullOrEmpty(this.QuestionKey))
+                    return RedirectToPage("/QuestionList"); // Redirect nếu không có QuestionKey
+                return Page();
+            }
+            else
+            {
+                return LocalRedirect("~/Warning?id=403");
+            }
+        }
+
+        #region [Record CRUD]
+        public IActionResult OnPostRecordRead([FromBody] ItemRequestAnswer request)
+        {
+            CheckAuth();
+            JsonResult zResult;
+            if (UserLogin.Role.IsRead)
+            {
+                AnswerDataAccess.Part1_Answer_Info zRecord;
+                if (string.IsNullOrEmpty(request.AnswerKey) || request.AnswerKey.Length != 36)
+                    zRecord = new AnswerDataAccess.Part1_Answer_Info();
+                else
+                    zRecord = new AnswerDataAccess.Part1_Answer_Info(request.AnswerKey);
+                zResult = new JsonResult(zRecord);
+            }
+            else
+            {
+                zResult = new JsonResult(new { Status = "ERROR", Result = "ACCESS DENIED" });
+            }
+            return zResult;
+        }
+
+        public IActionResult OnPostRecordCreate([FromBody] AnswerDataAccess.Part1_Answer_Info request)
+        {
+            CheckAuth();
+            JsonResult zResult;
+            if (UserLogin.Role.IsCreate)
+            {
+                AnswerDataAccess.Part1_Answer_Info zRecord = request;
+                if (string.IsNullOrEmpty(zRecord.QuestionKey) || !Guid.TryParse(zRecord.QuestionKey, out _))
+                {
+                    zResult = new JsonResult(new { status = "ERROR", message = "Invalid QuestionKey format" });
+                    return zResult;
+                }
+                zRecord.CreatedBy = UserLogin.Employee.Key;
+                zRecord.CreatedName = UserLogin.Employee.Name;
+                zRecord.Create();
+                zResult = new JsonResult(new { status = zRecord.Status, message = zRecord.Message });
+            }
+            else
+            {
+                zResult = new JsonResult(new { Status = "ERROR", Message = "ACCESS DENIED" });
+            }
+            return zResult;
+        }
+
+        public IActionResult OnPostRecordUpdate([FromBody] AnswerDataAccess.Part1_Answer_Info request)
+        {
+            CheckAuth();
+            JsonResult zResult;
+            if (UserLogin.Role.IsUpdate)
+            {
+                AnswerDataAccess.Part1_Answer_Info zRecord = request;
+                if (string.IsNullOrEmpty(zRecord.QuestionKey) || !Guid.TryParse(zRecord.QuestionKey, out _))
+                {
+                    zResult = new JsonResult(new { status = "ERROR", message = "Invalid QuestionKey format" });
+                    return zResult;
+                }
+                zRecord.ModifiedBy = UserLogin.Employee.Key;
+                zRecord.ModifiedName = UserLogin.Employee.Name;
+                zRecord.Update();
+                zResult = new JsonResult(new { status = zRecord.Status, message = zRecord.Message });
+            }
+            else
+            {
+                zResult = new JsonResult(new { Status = "ERROR", Message = "ACCESS DENIED" });
+            }
+            return zResult;
+        }
+
+        public IActionResult OnPostRecordDel([FromBody] ItemRequestAnswer request)
+        {
+            CheckAuth();
+            JsonResult zResult;
+            if (UserLogin.Role.IsDelete)
+            {
+                AnswerDataAccess.Part1_Answer_Info zRecord = new AnswerDataAccess.Part1_Answer_Info();
+                zRecord.AnswerKey = request.AnswerKey;
+                zRecord.Delete();
+                zResult = new JsonResult(new { status = zRecord.Status, message = zRecord.Message });
+            }
+            else
+            {
+                zResult = new JsonResult(new { Status = "ERROR", Message = "ACCESS DENIED" });
+            }
+            return zResult;
+        }
+        #endregion
+    }
+
+    public class ItemRequestAnswer
+    {
+        public string AnswerKey { get; set; }
+    }
+}
