9aff45621680b2b336312d7c75b09be9ad817d87
Le Quoc Nam <lequocnam012@gmail.com>
2025-03-14 14:04:50 +0700

    savechange1403EndPart1,2

---
+++ TNS_TOEICAdmin/TNS_TOEICAdmin/TNS_TOEICPart2/Areas/TOEICPart2/Pages/AnswerList.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TNS_TOEICPart2.Areas.TOEICPart2.Models;
+
+namespace TNS_TOEICPart2.Areas.TOEICPart2.Pages
+{
+    [IgnoreAntiforgeryToken]
+    public class AnswerListModel : PageModel
+    {
+        #region [Security]
+        public TNS.Auth.UserLogin_Info UserLogin;
+
+        private void CheckAuth()
+        {
+            UserLogin = new TNS.Auth.UserLogin_Info(User);
+            UserLogin.GetRole("TOEIC_Part2");
+            // For Testing
+            UserLogin.Role.IsRead = true;
+            UserLogin.Role.IsCreate = true;
+            UserLogin.Role.IsUpdate = true;
+            UserLogin.Role.IsDelete = true;
+        }
+        #endregion
+
+        public string QuestionKey { get; set; }
+
+        public IActionResult OnGet(string key)
+        {
+            CheckAuth();
+            if (UserLogin.Role.IsRead)
+            {
+                QuestionKey = key;
+                if (string.IsNullOrEmpty(QuestionKey))
+                    return LocalRedirect("~/TOEICPart2/QuestionList");
+                return Page();
+            }
+            else
+            {
+                return LocalRedirect("~/Warning?id=403");
+            }
+        }
+
+        public IActionResult OnPostLoadData([FromBody] ItemRequestKey request)
+        {
+            CheckAuth();
+            JsonResult zResult = new JsonResult("");
+            if (UserLogin.Role.IsRead)
+            {
+                zResult = AnswerListDataAccess.GetList(request.QuestionKey);
+            }
+            else
+            {
+                zResult = new JsonResult(new { Status = "ERROR", Result = "ACCESS DENIED" });
+            }
+            return zResult;
+        }
+    }
+
+    public class ItemRequestKey
+    {
+        public string QuestionKey { get; set; }
+    }
+}
