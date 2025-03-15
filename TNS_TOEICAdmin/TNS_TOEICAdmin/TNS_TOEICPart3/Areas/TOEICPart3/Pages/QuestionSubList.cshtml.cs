5b63c1f332b2ee9ebc2de40d98db8af621f53ad7
Le Quoc Nam <lequocnam012@gmail.com>
2025-03-15 17:19:31 +0700

    saveafternoon1503

---
+++ TNS_TOEICAdmin/TNS_TOEICAdmin/TNS_TOEICPart3/Areas/TOEICPart3/Pages/QuestionSubList.cshtml.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data;
+using System;
+using TNS_TOEICPart3.Areas.TOEICPart3.Models;
+
+namespace TNS_TOEICPart3.Areas.TOEICPart3.Pages
+{
+    [IgnoreAntiforgeryToken]
+    public class QuestionSubListModel : PageModel
+    {
+        #region [ Security ]
+        public TNS.Auth.UserLogin_Info UserLogin;
+        private void CheckAuth()
+        {
+            UserLogin = new TNS.Auth.UserLogin_Info(User);
+            UserLogin.GetRole("TOEIC_Part3");
+            //For Testing
+            UserLogin.Role.IsRead = true;
+            UserLogin.Role.IsCreate = true;
+            UserLogin.Role.IsUpdate = true;
+            UserLogin.Role.IsDelete = true;
+        }
+        #endregion
+
+        public string QuestionKey;
+        public IActionResult OnGet(string Key)
+        {
+            CheckAuth();
+            if (UserLogin.Role.IsRead)
+            {
+                QuestionKey = Key;
+                return Page();
+            }
+            else
+                return LocalRedirect("~/Warning?id=403");
+        }
+        public IActionResult OnPostLoadData([FromBody] ItemRequest request)
+        {
+            CheckAuth();
+            JsonResult zResult = new JsonResult("");
+            if (UserLogin.Role.IsRead)
+            {
+                zResult = QuestionSubListAccessData.GetList(request.QuestionKey);
+            }
+            else
+            {
+                zResult = new JsonResult(new { Status = "ERROR", Result = "ACCESS DENIED" });
+            }
+            return zResult;
+        }
+
+        public class ItemRequest
+        {
+            public string QuestionKey { get; set; }
+        }
+
+    }
+   
+}
